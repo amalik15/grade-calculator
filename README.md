3cd14398dd58da00dd1f79be6cced559fe21b44b
amalik15 <nitroflask@gmail.com>
2024-10-31 01:08:02 -0500

    update readme

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # grade-calculator
-(view in RAW)
-this is a weighted grade calculator
 
-it will take user input in the form of strings.
+This is a weighted grade calculator written in C.
 
-a string beginning '$' means that that string is a category
-Example: $homework 0.15
-    category: homework
-    weight of overall grade: 15%
-the number after the decimal must be a DECIMAL
+## Overview
 
-after the category string is inputted grades for that category should be inputted (one grade per line).
-The grades can be inputted as a fraction, decimal or percent. 
+The calculator takes user input in the form of strings to categorize and calculate grades.
 
-example: 50%
-         100%
-         24/30
-         0.85
-if entering a fraction, make sure to use whole numbers. otherwise enter the grade as a decimal or percent.
-Also when entering the grade as a percent, make sure to add the percent sign at the end.
+## Input Format
 
-Example user input:
+-   A string beginning with `$` indicates a category.
 
+    -   Example: `$homework 0.15`
+        -   Category: `homework`
+        -   Weight of overall grade: `15%`
+    -   The number after the decimal must be a **decimal**.
+
+-   After entering a category string, grades for that category should be inputted (one grade per line). The grades can be inputted as a fraction, decimal, or percent.
+
+### Examples
+
+-   Percent: `50%`, `100%`
+-   Fraction: `24/30`
+-   Decimal: `0.85`
+
+**Note:**
+
+-   When entering a fraction, use whole numbers.
+-   When entering a grade as a percent, add the percent sign at the end.
+
+## Example User Input
+
+```plaintext
 $homework 0.1
 24/30
 30/30
@@ -33,14 +42,35 @@ $homework 0.1
 30/30
 30/30
 30/30
-$midtrm1 0.2
+$midterm1 0.2
 21/40
 $midterm2 0.2
 39/50
 $final 0.5
 53/100
+```
 
-result: 61.800%
+## How to Run
 
-         
+To run the `main.c` file, follow these steps:
 
+1. **Compile the program**:
+
+    ```sh
+    gcc -o grade_calculator main.c
+    ```
+
+2. **Run the executable**:
+    ```sh
+    ./grade_calculator
+    ```
+
+Make sure you have `gcc` installed on your system. If not, you can install it using your package manager.
+
+For example, on Ubuntu, you can install `gcc` using:
+
+```sh
+sudo apt-get install gcc
+```
+
+On Windows, you can use MinGW to install `gcc`.
--- main.c
+++ main.c
@@ -1,9 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 typedef struct grade
 {
@@ -13,101 +12,98 @@ typedef struct grade
 	double weight;
 	struct grade *next;
 
-}grade;
-
+} grade;
 
 void Category(char input[], grade **root, char c[])
 {
-	
+
 	grade *cur = *root;
 	grade *pur = NULL;
-	
-	/*this will look through the nodes. It will print out and error message 
-	if the category is already exists and will return to main without creating a new node.*/
+
+	/*this will look through the nodes. It will print out and error message
+	if the category already exists and will return to main without creating a new node.*/
 	while (cur != NULL)
 	{
-		if (strcmp(input, cur->category) == 0) 
+		if (strcmp(input, cur->category) == 0)
 		{
 			printf("category already exists\n");
 			return;
 		}
-		
-		else 
+
+		else
 		{
 			pur = cur;
 			cur = cur->next;
 		}
 	}
-	/* if the while loop complets than that means the category is not found 
-	thus it will creat a new */
-	
-	
-	/*this block of code will loke for the first space in the input string 
+	/* if the while loop complets than that means the category is not found
+	thus it will creat a new one*/
+
+	/*this block of code will look for the first space in the input string
 	in order to find the seperation between the category name and it's weight*/
 	int i;
 	int length = strlen(input);
 	for (i = 0; i < length; i++)
 	{
-		if (input[i] == ' ') 
+		if (input[i] == ' ')
 		{
 			break;
 		}
 	}
-	
-	/*create a new vearable weight. Convert the part from the input string that is the 
-	weight into a double. sSet that double equal to the weight/*/ 
+
+	/*create a new vearable weight. Convert the part from the input string that is the
+	weight into a double. sSet that double equal to the weight/*/
 	double weight = atof(input + (i + 1));
-	
-	//allocate memory for a new node
-	grade *node = (grade*)malloc(sizeof(grade));
-	
-	//allocate memory and set the allocated memory to the name of the category
-	char *catagory = (char*)malloc(sizeof(char) * (i + 1));
+
+	// allocate memory for a new node
+	grade *node = (grade *)malloc(sizeof(grade));
+
+	// allocate memory and set the allocated memory to the name of the category
+	char *catagory = (char *)malloc(sizeof(char) * (i + 1));
 	strncpy(catagory, input + 1, i);
-	
-	//set values to new node
+
+	// set values to new node
 	node->category = catagory;
-	strcpy(c, catagory);   //update string c
+	strcpy(c, catagory); // update string c
 	node->weight = weight;
 	node->numberElm = 0;
 	node->sum = 0;
 	node->next = NULL;
-	
-	//add node to the end of the likned list
-	if (pur == NULL) 
+
+	// add node to the end of the likned list
+	if (pur == NULL)
 	{
 		*root = node;
 	}
-	
-	else 
+
+	else
 	{
 		pur->next = node;
 	}
 }
 
-
 void Fraction(char input[], grade **root, char c[])
 {
 	int i;
 	grade *cur = *root;
-	
-	//this while loop is used to stop cur at the desired category
+
+	// this while loop is used to stop cur at the desired category
 	while (cur != NULL)
 	{
-		if (strcmp(cur->category, c) == 0) 
+		if (strcmp(cur->category, c) == 0)
 		{
 			break;
 		}
-		
-		else 
+
+		else
 		{
 			cur = cur->next;
 		}
 	}
 
-	/*if cur is null, the category was not found and it will print an error message and return to main. 
+	/*if cur is null, the category was not found and it will print an error message and return to main.
 	this shouldn't be possible but I just put it as a precation*/
-	if (cur == NULL) 
+	if (cur == NULL)
 	{
 		printf("category not found\n");
 		return;
@@ -117,7 +113,7 @@ void Fraction(char input[], grade **root, char c[])
 
 	for (i = 0; i < length; i++)
 	{
-		if (input[i] == '/') 
+		if (input[i] == '/')
 		{
 			break;
 		}
@@ -126,8 +122,8 @@ void Fraction(char input[], grade **root, char c[])
 	i++;
 	double first = atof(input);
 	double secound = atof(input + i);
-	
-	//if the second number is zero, the fraction will not be added
+
+	// if the second number is zero, the fraction will not be added
 	if (secound == 0)
 	{
 		printf("improper fraction\n");
@@ -135,81 +131,80 @@ void Fraction(char input[], grade **root, char c[])
 	}
 
 	double fraction = first / secound;
-	
-	//update node for that category
+
+	// update node for that category
 	cur->sum = (cur->sum) + fraction;
 	cur->numberElm = (cur->numberElm) + 1;
 }
-
 
 void Decimal(char input[], grade **root, char c[])
 {
 	int i;
 	grade *cur = *root;
-	
-	//this while loop is used to stop cur at the desired category
+
+	// this while loop is used to stop cur at the desired category
 	while (cur != NULL)
 	{
-		if (strcmp(cur->category, c) == 0) 
+		if (strcmp(cur->category, c) == 0)
 		{
 			break;
 		}
-		
-		else 
+
+		else
 		{
 			cur = cur->next;
 		}
 	}
-	
-	/*if cur is null, the category was not found and it will print an error message and return to main. 
+
+	/*if cur is null, the category was not found and it will print an error message and return to main.
 	this shouldn't be possible but I just put it as a precation*/
-	if (cur == NULL) 
+	if (cur == NULL)
 	{
 		printf("category not found\n");
 		return;
 	}
 
 	double decimal = atof(input);
-	
-	//update node for that category
+
+	// update node for that category
 	cur->sum = (cur->sum) + decimal;
 	cur->numberElm = (cur->numberElm) + 1;
 }
-
 
 void Percent(char input[], grade **root, char c[])
 {
 	int i;
 	grade *cur = *root;
-	
-	//this while loop is used to stop cur at the desired category
+
+	// this while loop is used to stop cur at the desired category
 	while (cur != NULL)
 	{
-		if (strcmp(cur->category, c) == 0) 
+		if (strcmp(cur->category, c) == 0)
 		{
 			break;
 		}
-		
-		else 
+
+		else
 		{
 			cur = cur->next;
 		}
 	}
 
 	/*if cur is null, the category was not found. this shouldn't be possible but it is just a precation*/
-	if (cur == NULL) {
+	if (cur == NULL)
+	{
 		printf("category not found\n");
 		return;
 	}
 
 	double percent = (atoi(input)) / 100.0;
-	
-	//update node for that category
+
+	// update node for that category
 	cur->sum = (cur->sum) + percent;
 	cur->numberElm = (cur->numberElm) + 1;
 }
 
-//function that would return 1 if the string inputed is a category
+// function that would return 1 if the string inputed is a category
 int isCategory(char input[])
 {
 	char category = '$';
@@ -217,7 +212,7 @@ int isCategory(char input[])
 
 	ret = strchr(input, category);
 
-	if (ret == NULL) 
+	if (ret == NULL)
 	{
 		return 0;
 	}
@@ -225,153 +220,149 @@ int isCategory(char input[])
 	return 1;
 }
 
-
-//function that would return 1 if the string inputed is a function
+// function that would return 1 if the string inputed is a function
 int isFraction(char input[])
 {
 	char divison = '/';
 	char *ret;
-	
-	//looks for a "/" in the string inputed or returns null if it doesn't find it
+
+	// looks for a "/" in the string inputed or returns null if it doesn't find it
 	ret = strchr(input, divison);
 
-	if (ret == NULL) 
+	if (ret == NULL)
 	{
-		//return false
+		// return false
 		return 0;
 	}
-	
-	//return true
+
+	// return true
 	return 1;
 }
 
-//function that would return 1 if the string inputed is a decimal
+// function that would return 1 if the string inputed is a decimal
 int isDecimal(char input[])
 {
 	char decimal = '.';
 	char *ret;
-	
-	//looks for a "." in the string inputed or returns null if it doesn't find it
+
+	// looks for a "." in the string inputed or returns null if it doesn't find it
 	ret = strchr(input, decimal);
 
-	if (ret == NULL) 
+	if (ret == NULL)
 	{
-		//return false
+		// return false
 		return 0;
 	}
-	
-	//return true
+
+	// return true
 	return 1;
 }
 
-//function that would return 1 if the string inputed is a percent
+// function that would return 1 if the string inputed is a percent
 int isPercent(char input[])
 {
 	char percent = '%';
 	char *ret;
 
-	//looks for a "%" in the string inputed or returns null if it doesn't find it
+	// looks for a "%" in the string inputed or returns null if it doesn't find it
 	ret = strchr(input, percent);
 
-	if (ret == NULL) 
+	if (ret == NULL)
 	{
-		//return false
+		// return false
 		return 0;
 	}
-	
-	//return true
+
+	// return true
 	return 1;
 }
 
-//calculate and prints out grade
+// calculate and prints out grade
 void printGrade(grade *root)
 {
 
 	grade *cur = root;
 	double score = 0.0;
 	double weight = 0.0;
-	double grade; 
-	
-	//sums out all the weights and the weighted grades for each individual category 
+	double grade;
+
+	// sums out all the weights and the weighted grades for each individual category
 	while (cur != NULL)
 	{
-		score = score + (((cur->sum) / (cur->numberElm))*cur->weight);
+		score = score + (((cur->sum) / (cur->numberElm)) * cur->weight);
 		weight = weight + cur->weight;
 		cur = cur->next;
 	}
-	
-	//cheacks if the wight is not zero
-	if(weight != 0.0)
+
+	// cheacks if the wight is not zero
+	if (weight != 0.0)
 	{
 		grade = score / weight;
 	}
-	
-	//makes the grade 0 if the weight total is 0
+
+	// makes the grade 0 if the weight total is 0
 	else
 	{
 		grade = 0.0;
 	}
-	
-	
-	//prints grde with 3 significant digits
-	printf("Grade: %.3lf%%\n", (grade*100));
-	
-}
 
+	// prints grde with 3 significant digits
+	printf("Grade: %.3lf%%\n", (grade * 100));
+}
 
 int main()
 {
 	printf("This is a weighted grade calculator\n");
 
 	grade *Node = NULL;
-	char c[256]; 
+	char c[256];
 
 	char input[256];
 	int linesize = sizeof(input) / sizeof(input[0]);
 
-	//get user input
+	// get user input
 	printf(">> enter a grade or a new category (Enter to quit): ");
 	fgets(input, linesize, stdin);
 	input[strcspn(input, "\r\n")] = '\0';
-	
-	//checks if the user only pressed  the return key
+
+	// checks if the user only pressed  the return key
 	while (strlen(input) > 0)
 	{
-		//checks is the string inputted is a category
+		// checks is the string inputted is a category
 		if (isCategory(input) == 1)
 		{
-			//call a function to add category 
+			// call a function to add category
 			Category(input, &Node, c);
 		}
-		
-		//checks is the string inputted is a Fraction
+
+		// checks is the string inputted is a Fraction
 		else if (isFraction(input) == 1)
 		{
-			//call a function to convert string to double
+			// call a function to convert string to double
 			Fraction(input, &Node, c);
 		}
-		
-		//checks is the string inputted is a Decimal
+
+		// checks is the string inputted is a Decimal
 		else if (isDecimal(input) == 1)
 		{
-			//call a function to convert string to double
+			// call a function to convert string to double
 			Decimal(input, &Node, c);
 		}
-		
-		//checks is the string inputted is a Percent
+
+		// checks is the string inputted is a Percent
 		else if (isPercent(input) == 1)
 		{
-			//call a function to convert string to double
+			// call a function to convert string to double
 			Percent(input, &Node, c);
 		}
-		
-		//get next user input
+
+		// get next user input
 		printf(">> enter a grade or a new category (Enter to quit): ");
 		fgets(input, linesize, stdin);
 		input[strcspn(input, "\r\n")] = '\0';
 	}
-	
-	//call a function  to calculate and print grade
+
+	// call a function  to calculate and print grade
 	printGrade(Node);
 
 	return 0;
